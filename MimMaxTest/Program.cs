85b62cd24082a593f3b8f981d41e43f092e6d69d
Grant Magella <Bitman2003@gmail.com>
2023-08-18 10:03:00 -0400

    second problem added

--- MimMaxTest/Program.cs
+++ MimMaxTest/Program.cs
@@ -17,5 +17,20 @@
             }
         }
         Console.WriteLine($"Min: {min}, Max: {max}");
+        GetAverage(nbrs);
+    }
+
+    private static void GetAverage(int[] nbrs)
+    {
+        //int[] nbrs = { 754, 233, 509, 792, 700, 596, 833, 658, 998, 742, 187, 754, 308, 914, 489, 867, 717, 586, 929, 467, 460, 241, 770, 324, 599, 259, 120, 800, 336, 609, 690, 134, 598, 249, 282, 574, 334, 956, 659, 214, 435, 643, 809, 874, 906, 620, 328, 369, 426, 561 };
+
+        int sum = 0;
+        int avg = 0;
+        foreach (int x in nbrs)
+        {
+            sum += x;
+        }
+        avg=sum / nbrs.Length;
+        Console.WriteLine($"Sum: {sum}, Avg: {avg}");
     }
 }
